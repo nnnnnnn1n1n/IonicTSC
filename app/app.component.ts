51a3e750c70540a31e1fe2c920467f79fc9faec4
nnnnnnn1n1n <natjiraanankul@gmail.com>
2022-05-19 12:31:38 +0700

    7 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,16 +4,15 @@ import { Platform } from 'ionic-angular';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: 'app.html',
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage: any = TabsPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-
     });
   }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -6,30 +6,13 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+ import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+   declarations: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+entryComponents: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
+  providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }],
 })
 export class AppModule {}
